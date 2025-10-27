eaee15331dcea21659a5f736d698594ca3b7f114
Alex8473 <alexander.loos456@gmail.com>
2025-10-27 17:48:21 +0100

    Delete backend directory -

--- backend/routes/authRoutes.js
+++
@@ -1,6 +0,0 @@
-import { registerUser, loginUser } from "../controllers/authController.js";
-
-export const authRoutes = (app) => {
-  app.post("/register", registerUser);
-  app.post("/login", loginUser);
-};
--- backend/server.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { authRoutes } from "./routes/authRoutes.js";
-
-const app = express();
-app.use(express.json());
-
-authRoutes(app);
-
-app.listen(3000, () => console.log("Server läuft auf http://localhost:3000"));
